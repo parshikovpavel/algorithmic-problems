0b18126ab33dc23a09a48f398cfa544437f54b5b
pavelparshikov <paw-p@yandex.ru>
2020-07-04 20:02:39 +0300

    DatabaseTestCase.php phpunit.xml Combine Two Tables

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 
 1. [(1) Two Sum](src/TwoSum/description.md)
 2. [(2) Add Two Numbers](src/AddTwoNumbers/description.md)
-
 1. [(22) Generate Parentheses (правильные скобочные последовательности)](src/GenerateParentheses/description.md)
+4. [(175) Combine Two Tables]()
 
 
---
+++ tests/DatabaseTestCase.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ppAlgorithm;
+
+use PDO;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
+
+abstract class DatabaseTestCase extends TestCase
+{
+    use TestCaseTrait;
+
+    // сохранить соединения для других Test case's
+    static private $pdo = null;
+
+    // сохранить экземпляр PHPUnit\DbUnit\Database\Connection для других Test case's
+    private $conn = null;
+
+    final public function getConnection()
+    {
+        if ($this->conn === null) {
+            if (self::$pdo === null) {
+                self::$pdo = new PDO( $GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'] );
+            }
+            $this->conn = $this->createDefaultDBConnection(self::$pdo, $GLOBALS['DB_DBNAME']);
+        }
+
+        return $this->conn;
+    }
+}
